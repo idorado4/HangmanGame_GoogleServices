cd7823f539c0e445e8dad8876aeb92ee8c8f00e2
Ian Dorado Rivillas <ian.dorado@enti.cat>
2021-12-16 23:07:49 +0100

    Login con User Pass + Encrypt Password

--- Assets/Project/Code/Model/UseCases/NewUserPasswordLoginUseCase.cs
+++ Assets/Project/Code/Model/UseCases/NewUserPasswordLoginUseCase.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class UserPasswordLoginUseCase : IUserPasswordLoginUseCase
 {
+
     public async void Do(string email, string password)
     {
         await ServiceLocator.Instance.GetService<ILoginService>().UserPasswordLogin(email, password);
